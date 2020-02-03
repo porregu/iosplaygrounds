7e1499b8893924e79a0bc481f6728c3c8f9ab054
guillermo <porregu@ssfs.org>
2020-02-03 14:11:25 -0500

    finish functions

---
+++ Functions.playground/Pages/Hiding Complexity.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+ ## Hiding Complexity
+ 
+ It might seem that printing the nursery rhyme using functions is both longer and more complicated than the original code on the first page of this playground. If you attempt to think about every single function at the same time, then it is. But the point of using functions is to break things down into understandable, reusable parts. Each part does a very clear piece of work.
+ 
+ When working on an app, you’re never looking at every single line of code. You’ll call a function knowing _what_ it does, but not necessarily _how_ it does it. For example, in this page all of the song creation functions have been built into the page already, so you can write the song very simply:
+*/
+verseOne()
+breatheBetweenVerses()
+verseTwo()
+breatheBetweenVerses()
+verseThree()
+breatheBetweenVerses()
---
+++ Functions.playground/Pages/Meme Exercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+func firstVerse() {
+    print(" feliz cumpleanos ")
+    print(" para ti ")
+}
+func secodnVerse() {
+    print("te deseamos todos ")
+    print(" un feliz cumpleanos ")
+}
+func threVerse() {
+    print(" claps!!!! ")
+}
+firstVerse()
+threVerse()
+secodnVerse()
+threVerse()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//: Next, make the meme your own.
+//:
+//: [Previous](@previous)  |  page 11 of 12  |  [Next: Personal Meme](@next)
