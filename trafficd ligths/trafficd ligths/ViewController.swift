34ed60c49c0a224fff16475f52acb3ea7c22abe6
guillermo <porregu@ssfs.org>
2020-02-13 14:53:20 -0500

    trqaffic lights

---
+++ trafficd ligths/trafficd ligths/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  trafficd ligths
+//
+//  Created by Guillermo on 2/13/20.
+//  Copyright © 2020 Guillermo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func returnToFirstScreen(uniwindsegue: UIStoryboardSegue){
+        
+    }
+
+}
+
