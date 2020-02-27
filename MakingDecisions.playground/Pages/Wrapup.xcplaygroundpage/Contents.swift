a9a31d4c99e3034e99cf0508a1e46bed6425c310
guillermo <porregu@ssfs.org>
2020-02-27 14:56:12 -0500

    finish making decissions

---
+++ MakingDecisions.playground/Pages/Wrapup.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*:
+ ## Wrapup
+ 
+ - Making decisions and choosing which code to run based on those decisions adds power to your code.
+ - Decisions can be made by comparison statements, which can have a value of either `true` or `false`
+ - Comparison statements can be used to run parts of code with the use of `if` and `else`
+ 
+ ```
+ let youWantToPractice = true
+ if youWantToPractice {
+    Go to the exercises!
+ }
+ ```
+ 
+[Previous](@previous)  |  page 10 of 13  |  [Next: Exercise: If Statement Practice](@next)
+*/
+let youWantToeat = true
+if youWantToeat {
+   "lets go to the restaurant!"
+}
