681374cc4964f11c9e411a9f2af94c5024541a1e
guillermo <porregu@ssfs.org>
2020-03-12 09:33:27 -0400

    add emoji dictionaru

---
+++ emojidictionary/emojidictionary/Emoji.swift
@@ -0,0 +1,23 @@
+//
+//  Emoji.swift
+//  emojidictionary
+//
+//  Created by Guillermo on 3/11/20.
+//  Copyright © 2020 Guillermo. All rights reserved.
+//
+
+import Foundation
+
+struct emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+init(symbol: String, name: String, description: String,
+usage: String) {
+    self.symbol = symbol
+    self.name = name
+    self.description = description
+    self.usage = usage
+    }
+}
---
+++ emojidictionary/emojidictionary/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  emojidictionary
+//
+//  Created by Guillermo on 3/11/20.
+//  Copyright © 2020 Guillermo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    }
+
+
+
