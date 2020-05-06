3ee44f46c1eef11fb52e564cea6de3d77ddb0780
guillermoporresmarin <porregu@ssfs.org>
2020-05-06 10:12:51 -0400

    work on the image

---
+++ finalproject/finalproject/Carpet.swift
@@ -0,0 +1,14 @@
+//
+//  Carpet.swift
+//  finalproject
+//
+//  Created by guillermo porres marin  on 5/6/20.
+//  Copyright © 2020 guillermo porres marin . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Carpet{
+    var images: [UIImage]
+}
--- finalproject/finalproject/ViewController.swift
+++ finalproject/finalproject/ViewController.swift
@@ -8,12 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    @IBOutlet var carpets: [UIButton]!
+    var newCarpets = Carpet(images: [UIImage]())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerController.delegate = self
         // Do any additional setup after loading the view.
     }
 
+    let pickerController = UIImagePickerController()
+
+    
+    @IBAction func Takenewpic(_ sender: UIButton) {
+        pickerController.allowsEditing = false
+        pickerController.sourceType = .photoLibrary
+
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        //var newImage: UIImage
+        //if let possibleImage = info[.editedImage] as? UIImage {
+        //    newImage = possibleImage
+        //} else if let possibleImage = info[.originalImage] as? UIImage {
+        //    newImage = possibleImage
+        //} else {
+        //    return
+        //}
+
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//            picture.contentMode = .scaleAspectFit
+//            picture.image = pickedImage
+            
+            // TODO: create new carpet
+            // add the image to the carpet images array
+            for carpet in carpets {
+                if carpet.currentImage == nil {
+                    carpet.setImage(pickedImage, for: .normal)
+                    break
+                }
+            }
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
